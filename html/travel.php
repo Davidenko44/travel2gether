a74491e005f11c87f63bce401d8572fd365137e8
Davidenko44 <david_rip@hotmail.com>
2015-03-17 21:08:44 +0100

    Amazing form

--- html/travel.php
+++ html/travel.php
@@ -1,3 +1,23 @@
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+
+<head>
+	<title>NS train planner</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+	<table>
+		<thead>
+			<tr>
+				<th>Aantal Overstappen</th>
+				<th>ReisTijd</th>
+				<th>VertrekTijd</th>
+				<th>AankomstTijd</th>
+				<th>Vrienden op deze rit</th>
+			</tr>
+		</thead>
+		<tbody>
 <?php
 
 function getStationCodeByName($station) {
@@ -34,10 +54,26 @@ function travelAdvice($from, $to) {
 	curl_close($ch);
 	
 	if ($info['http_code'] === 200) {
-		echo $output;
+		$travelOptions = simplexml_load_string($output) or die("Could not load results");
+		foreach ($travelOptions->ReisMogelijkheid as $travelOption) { ?>
+			<tr>
+				<td><?php echo $travelOption->AantalOverstappen ?></td>
+				<td><?php echo $travelOption->ActueleReisTijd ?></td>
+				<td><?php echo date_format(date_create($travelOption->ActueleVertrekTijd), 'H:i') ?></td>
+				<td><?php echo date_format(date_create($travelOption->ActueleAankomstTijd), 'H:i') ?></td>
+				
+				<!-- ROBIN: hier kun je aan de hand van $travelOption en zijn values de ritten vinden (en dus de vrienden) -->
+				<!-- met het uitvoeren van var_dump($travelOption); kun je zien welke values hij heeft -->
+			</tr>
+		<?php
+		}
 	}
 }
 
 travelAdvice(getStationCodeByName($_POST['from']), getStationCodeByName($_POST['to']));
 
 ?>
+		</tbody>
+	</table>
+</body>
+</html>
