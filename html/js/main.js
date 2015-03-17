b4d69d5283795941f4d4492d06da2ea2d0b8a725
Davidenko44 <david_rip@hotmail.com>
2015-03-17 14:50:47 +0100

    added NS form
    non-working on localhost

--- html/js/main.js
+++ html/js/main.js
@@ -1,9 +1,18 @@
 function submitfunction() {
-	console.log()
-	queryNS();
-	return false;
+	var from = $("#from")[0], to = $("#to")[0];
+	
+	queryNS(from.value, to.value);
 }
 
-function queryNS() {
+function queryNS(fromStation, toStation) {
+	var url = "http://webservices.ns.nl/ns-api-treinplanner?fromStation=" + fromStation + "&toStation=" + toStation;
 	
+	console.log(url);
+	
+	$.ajax(url, {
+		complete: function (xmlhttprequest, response) {
+			console.log(response);
+			console.log(xmlhttprequest);
+		}
+	})
 }
